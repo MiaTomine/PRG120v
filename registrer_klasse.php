9571287ea2ad00380586f4886c320dd16f06289f
Mia Tomine <155964539+MiaTomine@users.noreply.github.com>
2025-09-26 11:07:22 +0200

    Create registrer_klasse.php

---
+++ registrer_klasse.php
@@ -0,0 +1,19 @@
+<?php
+include("db-tilkobling.php");
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $klassekode = $_POST['klassekode'];
+    $klassenavn = $_POST['klassenavn'];
+    $studiumkode = $_POST['studiumkode'];
+
+    $sql = "INSERT INTO klasse (klassekode, klassenavn, studiumkode) VALUES ('$klassekode', '$klassenavn', '$studiumkode')";
+    
+    if (mysqli_query($db, $sql)) {
+        echo "Klasse registrert vellykket.";
+    } else {
+        echo "Feil: " . mysqli_error($db);
+    }
+}
+
+mysqli_close($db);
+?>
